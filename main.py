9f7edef777f11cf18221e1c292d83e03a52b1438
Abdulaziz <ibabdulaziz2197@gmail.com>
2026-03-23 15:18:20 +0500

    Add multiple functions manipulating global variables

---
+++ main.py
@@ -0,0 +1,104 @@
+# 1
+son = 10
+
+def oshir():
+    global son
+    son = son + 5
+
+oshir()
+print(son)
+
+# 2
+yosh = 14
+
+def oshir():
+    global yosh
+    yosh = yosh * 2
+
+oshir()
+print(yosh)
+
+3
+son = 100
+
+def oshir():
+    global son
+    son = son + 50
+
+oshir()
+print(son)
+
+
+# 4
+# 1
+son = 10
+
+def oshir():
+    global son
+    son = son + 5
+
+oshir()
+print(son)
+
+
+# 5
+son = 30
+
+def oshir():
+    global son
+    son = son * 2
+
+oshir()
+print(son)
+
+# 6
+son = 200
+
+def bol():
+    global son
+    son = son / 5
+
+bol()
+print(son)
+
+# 7
+son = 6
+
+def oshir():
+    global son
+    son = son * 6
+
+oshir()
+print(son)
+
+# 8
+son = 12
+
+def oshir():
+    global son
+    son = son + 3
+
+oshir()
+print(son)
+
+
+# 9
+harf = "A"
+
+def qosh():
+    global harf
+    harf = harf + "B"
+
+qosh()
+print(harf)
+
+
+# 10
+son = 50
+
+def kamaytir():
+    global son
+    son = son - 10
+
+kamaytir()
+print(son)
